1f92f4045ad39acd649fbc0b76f72b41fb1d8180
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-08-18 18:47:39 +0930

    added getDog for single dog, and addDog for adding a dog

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -7,5 +7,27 @@ module.exports = {
     const user = await User.findById(id);
 
     return res.json(user.dogs);
+  },
+
+  //get a single dog
+  getDog: async (req,res) => {
+    const {userId, dogId} = req.body;
+    const user = await User.findById(userId);
+
+    const dog = user.dogs.id(dogId);
+
+    return res.json(dog);
+  },
+
+  //add dog
+  addDog: async (req, res) => {
+    const {userId, name, dob, breed} = req.body;
+    let user = await User.findById(userId);
+    
+    const newDog = {name, dob, breed};
+    user.dogs.push(newDog);
+    user = await user.save();
+
+    return res.status(200).json(user.dogs)
   }
 }
--- routes/api/dogRoutes.js
+++ routes/api/dogRoutes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const {
   getDogs,
+  getDog,
+  addDog,
 } = require('../../controllers/dogController');
 
 router.route('/all').get(getDogs);
+router.route('/single').get(getDog);
+router.route('/add').post(addDog)
 
 module.exports = router;
